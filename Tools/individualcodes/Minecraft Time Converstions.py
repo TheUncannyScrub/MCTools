4078cc248260cf52c4345a8cf7c149f769ffef64
TheUncannyScrub <scotty.dot@live.co.uk>
2017-06-04 16:31:01 +0100

    Added individual Source code for all components of the main script

---
+++ Tools/individualcodes/Minecraft Time Converstions.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import random
+import math
+from time import *
+import decimal
+print('Hello World')
+print('-')
+print('Times Converstions in Minecraft')
+print('-')
+sleep(1)
+print('MC Time to Real Time')
+print('1 Minute = 0.8 Seconds')
+print('1 Hour = 50 Seconds')
+print('1 Day = 20 Minutes')
+print('1 Week = 2.3 Hours')
+print('1 Month = 10 Hours')
+print('1 Year = 5 Days')
+print('-')
+print('Day Time = 10 Minutes')
+print('Sunset/Dusk = 1.5 Minutes')
+print('Night Time = 7 Minutes')
+print('Sunrise/dawn = 1.5 Minutes')
+print('-')
+
+sleep(1)
+input('Press ENTER to exit')
